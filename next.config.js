99100502a000420323037bc7d61488bd5df30c63
ClarysV <139833602+ClarysV@users.noreply.github.com>
2023-07-19 16:38:45 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,13 +7,7 @@ const nextConfig = {
 
   webpack(config, { isServer, dev }) {
 
-    process.env.OPENAI_API_KEY = "824dc9d1708b45219043148791cab850"
-    process.env.OPENAI_API_HOST = "https://cweaoaiagp04openai01.openai.azure.com/"
-    process.env.OPENAI_API_BASE = "https://cweaoaiagp04openai01.openai.azure.com/"
-    process.env.OPENAI_API_TYPE = "azure"
-    process.env.OPENAI_API_VERSION = "2023-03-15-preview"
-    process.env.AZURE_DEPLOYMENT_ID = "qa_model"
-    process.env.DEFAULT_MODEL = "gpt-35-turbo"
+
 
     config.experiments = {
       asyncWebAssembly: true,
