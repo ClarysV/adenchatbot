1eaed7e47550c430ca8e6529f0be21bc823cd56a
ClarysV <139833602+ClarysV@users.noreply.github.com>
2023-07-19 14:37:29 +0800

    Update env.ts

--- types/env.ts
+++ types/env.ts
@@ -1,7 +1,7 @@
 export interface ProcessEnv {
-  OPENAI_API_KEY: "824dc9d1708b45219043148791cab850";
-  OPENAI_API_HOST?: "https://cweaoaiagp04openai01.openai.azure.com/";
-  OPENAI_API_TYPE?: 'azure';
-  OPENAI_API_VERSION?: "2023-03-15-preview";
+  OPENAI_API_KEY: string;
+  OPENAI_API_HOST?: string;
+  OPENAI_API_TYPE?: 'openai' | 'azure';
+  OPENAI_API_VERSION?: string;
   OPENAI_ORGANIZATION?: string;
 }
