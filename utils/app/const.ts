c492612716acd2159cc78379ff321dfc88bacc67
ClarysV <139833602+ClarysV@users.noreply.github.com>
2023-07-19 14:43:56 +0800

    Update const.ts

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -3,13 +3,13 @@ export const DEFAULT_SYSTEM_PROMPT =
   "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
 
 export const OPENAI_API_HOST =
-  process.env.OPENAI_API_HOST || 'https://api.openai.com';
+  process.env.OPENAI_API_HOST || 'https://cweaoaiagp04openai01.openai.azure.com/';
 
 export const DEFAULT_TEMPERATURE = 
   parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "1");
 
 export const OPENAI_API_TYPE =
-  process.env.OPENAI_API_TYPE || 'openai';
+  process.env.OPENAI_API_TYPE || 'azure';
 
 export const OPENAI_API_VERSION =
   process.env.OPENAI_API_VERSION || '2023-03-15-preview';
